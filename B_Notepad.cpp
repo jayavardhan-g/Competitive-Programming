32a1b0d2be8b69fb645e70c4eb1fef6d9cc15e85
jayavardhan-nani <jayavardhan.gollapalli@gmail.com>
2022-12-12 22:12:05 +0530

    12-12-22

---
+++ B_Notepad.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+//#define int long long
+int32_t main(){
+    int t;cin>>t;
+    while(t--){
+        int n;cin>>n;
+        string s;
+        cin>>s;
+        int x=0;
+        if(n==2||n==1||n==3){cout<<"NO"<<endl;continue;}
+        for(int i=3;i<n-1;i++){ 
+            string a=s.substr(0,i);
+            string b=s.substr(i,2);
+            x=a.find(b);
+            if(x!=-1)break;
+
+        }
+        if(x!=-1)cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
+}
--- test.cpp
+++ test.cpp
@@ -2,5 +2,8 @@
 using namespace std;
 #define int long long
 int32_t main(){
-    
+    string s="Hello world";
+    string t="wr";
+    int l=s.find(t);
+    cout<<l;  
 }
