b472040085ca6f62238ba60aa690b9792a8ec705
jayavardhan-nani <jayavardhan.gollapalli@gmail.com>
2022-12-14 19:01:31 +0530

    14-12-22

--- test.cpp
+++ test.cpp
@@ -1,10 +1,17 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define int long long
+int recursive(int x,int y){
+    if(y==1)return x;
+    if(y%2==0){
+        x=power(x*x,y/2);
+    }else{
+        x=x*power(x*x,y/2);
+    }
+    return x;
+}
 int32_t main(){
-    string s="world";
-    string t=s.substr(4,2);
-    cout<<t;
-    int l=s.find(t);
-    cout<<l;
+    int l;cin>>l;
+    int x;cin>>x;
+    cout<<recursive(l,x);
 }
